73c18b79c7281759b038c7bb8d01dee1492b408c
Brian Abelson <brianabelson@gmail.com>
2013-11-27 19:33:51 -0500

    added image support

--- meatwaves.py
+++ meatwaves.py
@@ -1,10 +1,12 @@
 from socketIO_client import SocketIO
 import re
 import requests
-import tweepy
+from twython import Twython
 import os
-
-
+import base64
+from PIL import Image
+import StringIO
+import subprocess
 
 # Listening to meatspac, sending back to staging for now
 # This can be just an ADDRESS variable when/if listening to meatspac and posting back
@@ -16,40 +18,70 @@ M2T = re.compile("^MT(:)?")
 class MeatWaves(object):
 
     def __init__(self):
-        self.consumer_key = os.getenv('MT_CONSUMER_KEY')
-        self.consumer_secret = os.getenv('MT_CONSUMER_SECRET')
-        self.access_token = os.getenv('MT_ACCESS_TOKEN')
-        self.access_token_secret = os.getenv('MT_ACCESS_TOKEN_SECRET')
+      self.consumer_key = os.getenv('MT_CONSUMER_KEY')
+      self.consumer_secret = os.getenv('MT_CONSUMER_SECRET')
+      self.access_token = os.getenv('MT_ACCESS_TOKEN')
+      self.access_token_secret = os.getenv('MT_ACCESS_TOKEN_SECRET')
 
-        self.api = self.connect_to_twitter()
+      self.api = self.connect_to_twitter()
 
-        print "Listening to %s" % ADDRESS
-        with SocketIO(ADDRESS) as socketIO:
-            socketIO.on('message', self.on_message)
-            socketIO.wait()
+      print "Listening to %s" % ADDRESS2
+      with SocketIO(ADDRESS2) as socketIO:
+          socketIO.on('message', self.on_message)
+          socketIO.wait()
 
     def connect_to_twitter(self):
-      auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
-      auth.set_access_token(self.access_token, self.access_token_secret)
-      return tweepy.API(auth)
+      api = Twython(
+        self.consumer_key,
+        self.consumer_secret,
+        self.access_token,
+        self.access_token_secret
+        )
+      return api
+
+    def format_media(self, b64):
+      # format string
+      b64 = b64.split('base64,')[1]
+
+      # pad string
+      b64 += "=" * ((4 - len(b64) % 4) % 4)
+
+      # decode string
+      data = base64.b64decode(b64)
+
+      # read in encoded data
+      f = StringIO.StringIO(data)
+
+      # get original height and width
+      im = Image.open(f)
+
+      # save image and open it up again, don't know why i have to do this, it's a hack...
+      im.save('img.gif')
+      media = open('img.gif', 'rb')
+
+      # remove image
+      subprocess.call(['rm', 'img.gif'])
+
+      return media
 
     def on_message(self, *args):
-        try:            
-            # Grab the message. Meatspac messages are nested dicts in the form:
-            # {u'chat': {u'value': {u'media': u'data:image/gif;base64,<wow    so base64   such image>', u'message': '<witicism here>', u'ttl': 600000, u'created': 1385499795344, u'fingerprint': u'93d944673197120b6d611d3014d81949'}, u'key': u'1385499795344!f72be0da-83d7-4bff-8c6e-562928ae6162'}}
-            all_message_data = args[0]
-            message = all_message_data['chat']['value']['message'].strip()
+      try:            
+        # Grab the message. Meatspac messages are nested dicts in the form:
+        all_message_data = args[0]
+        message = all_message_data['chat']['value']['message'].strip()
+        b64 = all_message_data['chat']['value']['media']
 
-            m = M2T.search(message)
-            if m:
-              message = M2T.sub('', message).strip()
-              print message
-              try:
-                self.api.update_status(message)
-              except tweepy.TweepError:
-                pass
-        except:
-          pass
+        m = M2T.search(message)
+        if m:
+          message = M2T.sub('', message).strip()
+          print message
+          media = self.format_media(b64)
+          try:
+            self.api.update_status_with_media(status=message, media=media)
+          except tweepy.TweepError:
+            pass
+      except:
+        pass
 
 if __name__ == '__main__':
     mw = MeatWaves()
